1edf1408e55d8c566a645adc22c8d7cbc05c974a
José Rodrigues <joser0drigueswork@gmail.com>
2024-06-28 17:03:12 +0100

    Fix NMS for Craftbukkit

--- realskywars-api/src/main/java/joserodpt/realskywars/api/nms/ReflectionHelper.java
+++ realskywars-api/src/main/java/joserodpt/realskywars/api/nms/ReflectionHelper.java
@@ -17,22 +17,23 @@ package joserodpt.realskywars.api.nms;
 
 
 import joserodpt.realskywars.api.RealSkywarsAPI;
+import org.bukkit.Bukkit;
 
-public class ReflectionHelper
-{
+public class ReflectionHelper {
+    private static final String CRAFTBUKKIT_PACKAGE = Bukkit.getServer().getClass().getPackage().getName();
+
     public static Class<?> getNMSClass(final String str) {
         return getClass("net.minecraft.server." + RealSkywarsAPI.getInstance().getServerVersion() + "." + str);
     }
-    
+
     public static Class<?> getCraftBukkitClass(final String str) {
-        return getClass("org.bukkit.craftbukkit." + RealSkywarsAPI.getInstance().getServerVersion() + "." + str);
+        return getClass(CRAFTBUKKIT_PACKAGE + "." + str);
     }
-    
+
     public static Class<?> getClass(final String className) {
         try {
             return Class.forName(className);
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             RealSkywarsAPI.getInstance().getLogger().severe("Error while executing reflection (getClass) nms.");
             RealSkywarsAPI.getInstance().getLogger().severe(ex.toString());
             return null;
